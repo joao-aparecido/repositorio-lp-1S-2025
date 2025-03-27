b0f43fcc8abe42b2ffd69f0c7f971667e9c8b5b7
joao-aparecido <joaoaparecidoferreiraneto@gmail.com>
2025-03-27 12:41:58 -0300

    numericos

--- tipo_de_dados/numericos.c
+++ tipo_de_dados/numericos.c
@@ -2,6 +2,20 @@
 
 int main(int argc, char* argv[]){
     int idade = 30;
-    printf("A idade eh %d",idade); 
+    float altura = 1.72;
+    float peso = 79.85;
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+    getchar();
+    printf("Digite a altura: ");
+    scanf("%f", &altura);
+    printf("Digite seu peso: ");
+    scanf("%f", &peso);
+    getchar();
+    printf("A idade eh %d\n",idade); 
+    printf("a altura eh %.2f\nO peso eh %.2f\n",altura, peso);
+    
+    double dobro_peso = peso * 2.0;
+    printf("O dobro do peso eh %.2f", dobro_peso);
     return 0;
 }
