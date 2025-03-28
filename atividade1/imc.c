af569aacc91bac00c20e0e15d42edbabe8475647
joao-aparecido <joaoaparecidoferreiraneto@gmail.com>
2025-03-28 10:58:38 -0300

    imc

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -4,11 +4,15 @@ int main(int argc, char* argv[]){
     float altura;
     float peso;
     float imc;
+
     printf("Digite a altura: ");
     scanf("%f", &altura);
+
     printf("Digite seu peso: ");
     scanf("%f", &peso);
+
     imc = peso / (altura * altura);
     printf("Seu imc eh: %.2f", imc);
+    
     return 0;
 }
